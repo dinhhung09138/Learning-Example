69a390c811d9797ea77ebffba35d8ac7020c47fc
dinhhung09138 <dinhhung09138@gmail.com>
2021-06-30 22:08:45 +0700

    Learning Blazor - Partial class

---
+++ MyVilla/Movies.Client/Pages/Counter.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Movies.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject]
+        SingleTonService singleTon { get; set; }
+
+        [Inject] 
+        TransientService transient {get; set;}
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            singleTon.Value += 1;
+            transient.Value += 1;
+        }
+
+    }
+}
