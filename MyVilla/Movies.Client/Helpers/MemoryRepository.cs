c0946ed280a589a112139c792cc1be29ea05ff65
dinhhung09138 <dinhhung09138@gmail.com>
2021-07-02 21:58:01 +0700

    Learning Blazor - Link and Navigation

--- MyVilla/Movies.Client/Helpers/MemoryRepository.cs
+++ MyVilla/Movies.Client/Helpers/MemoryRepository.cs
@@ -12,17 +12,23 @@ namespace Movies.Client.Helpers
         {
             return new List<Movie>()
             {
-                new Movie(){
+                new Movie()
+                {
+                    Id = 1,
                     Title = "Spider Man",
                     ReleaseDate = new DateTime(2019, 7, 10),
                     Poster = "https://m.media-amazon.com/images/M/MV5BZTQyNTU0MDktYTFkYi00ZjNhLWE2ODctMzBkM2U1ZTk3YTMzXkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_FMjpg_UY337_.jpg",
                 },
-                new Movie(){
+                new Movie()
+                {
+                    Id = 2,
                     Title = "Loky - Marven Studio",
                     ReleaseDate= DateTime.Now,
                     Poster = "https://m.media-amazon.com/images/M/MV5BNTkwOTE1ZDYtODQ3Yy00YTYwLTg0YWQtYmVkNmFjNGZlYmRiXkEyXkFqcGdeQXVyNTc4MjczMTM@._V1_.jpg",
                 },
-                new Movie(){
+                new Movie()
+                {
+                    Id = 3,
                     Title = "Quiet Place",
                     ReleaseDate = DateTime.Now,
                     Poster = "https://m.media-amazon.com/images/M/MV5BMTE2ODU4NDEtNmRjNS00OTk1LTg4NmMtNTAzYzVlNzJmYjgzXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_FMjpg_UY473_.jpg",
--- MyVilla/Movies.Shared/Entities/Movie.cs
+++ MyVilla/Movies.Shared/Entities/Movie.cs
@@ -8,6 +8,8 @@ namespace Movies.Shared.Entities
 {
     public class Movie
     {
+        public int Id { get; set; }
+
         public string Title { get; set; }
 
         public DateTime ReleaseDate { get; set; }
