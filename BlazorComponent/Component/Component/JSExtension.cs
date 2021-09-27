3a05b0d9928864892119b12bf8222c3e596fb851
dinhhung09138 <dinhhung09138@gmail.com>
2021-09-27 14:06:15 +0700

    Blazor component

---
+++ BlazorComponent/Component/Component/JSExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Component
+{
+    public static class JSExtension
+    {
+    }
+}
---
+++ BlazorComponent/Component/Component/Pages/LearningBlazor/Button.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Component.Pages.LearningBlazor
+{
+    partial class Button : ComponentBase
+    {
+        public string value = "Hello world";
+
+    }
+}
