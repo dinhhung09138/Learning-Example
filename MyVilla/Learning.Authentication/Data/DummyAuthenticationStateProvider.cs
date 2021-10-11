576f75bbac199b2ba5f5cda79b2bda5e54efbc47
dinhhung09138 <dinhhung09138@gmail.com>
2021-10-11 14:18:22 +0700

    Demo project

---
+++ MyVilla/Learning.Authentication/Data/DummyAuthenticationStateProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Learning.Authentication.Data
+{
+    public class DummyAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(5000);
+            var anonymous = new ClaimsIdentity();
+
+            var user = new ClaimsPrincipal(anonymous);
+
+            return new AuthenticationState(user);
+        }
+    }
+}
